b37d5ea2bf166511ea6cb85cb5fcec90c946bf32
Serena Black <sunsetsin1.sy@gmail.com>
2023-01-23 18:37:22 +0545

    refactor: console.log removed

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -5,14 +5,6 @@ const prisma = new PrismaClient({ log: ["query"] });
 const bcrypt = require("bcryptjs");
 
 async function handler(req, res) {
-  if (req.method === "GET") {
-    if (req.user) {
-      let user = await prisma.user.findMany({});
-      console.log(user);
-      return res.status(200).json(user);
-    }
-  }
-
   if (req.method === "POST") {
     const {
       firstName,
@@ -24,10 +16,8 @@ async function handler(req, res) {
       gender,
       address,
     } = req.body.data;
-    console.log(firstName, lastName);
     const saltRounds = 10;
     const passwordHash = await bcrypt.hash(password, saltRounds);
-    console.log(firstName, lastName);
     const user = await prisma.user.create({
       data: {
         firstName: firstName,
@@ -40,9 +30,7 @@ async function handler(req, res) {
         address: address,
       },
     });
-    console.log(user);
-
     res.status(203).json({ user });
   }
 }
-export default tokenExtractor(handler);
+export default handler;
