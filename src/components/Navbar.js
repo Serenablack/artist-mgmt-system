31a67fd66dd873f0e9780b70f128e00df8c25628
Serena Black <sunsetsin1.sy@gmail.com>
2023-01-26 17:27:13 +0545

    fix: issue related to button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
 import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import Avatar from "@mui/material/Avatar";
-import { useRouter } from "next/router";
 
-export default function Navbar() {
+import { useRouter } from "next/router";
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+const Navbar = () => {
   const { push } = useRouter();
   let user;
   if (typeof window !== "undefined") {
@@ -20,47 +24,37 @@ export default function Navbar() {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Typography
-        variant="h6"
-        component="div"
-        alignItems="center"
-        sx={{ flexGrow: 1 }}
-      >
-        Artist Management System
-      </Typography>
-      <AppBar position="static">
-        <Toolbar>
-          <Box display="flex" justifyContent="split-pair">
-            <Box>
-              <Button color="#c223er" onClick={() => push("/create")}>
-                Create
+    <Container>
+      <Box sx={{ flexGrow: 1 }}>
+        <Typography
+          variant="h6"
+          component="div"
+          alignItems="center"
+          sx={{ flexGrow: 1 }}
+        >
+          Artist Management System
+        </Typography>
+        <AppBar position="static">
+          <Toolbar>
+            <Box display="flex" justifyContent="split-pair">
+              <Box>
+                <Button onClick={() => push("/user")}>Users</Button>
+                <Button onClick={() => push("/artist")}>Artist</Button>
+              </Box>
+            </Box>
+            <Box display="flex" alignItems="center" gap={1.5}>
+              <Avatar />
+              <Typography variant="overline">
+                {user?.firstName + " " + user?.lastName}
+              </Typography>
+              <Button variant="contained" onClick={handleLogout}>
+                Logout
               </Button>
             </Box>
-            <Box>
-              <Button color="inherit" onClick={() => push("/user")}>
-                Users
-              </Button>
-              <Button color="inherit" onClick={() => push("/artist")}>
-                Artist
-              </Button>
-            </Box>
-          </Box>
-          <Box display="flex" alignItems="center" gap={1.5}>
-            <Avatar />
-            <Typography variant="overline">
-              {user?.firstName + " " + user?.lastName}
-            </Typography>
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={handleLogout}
-            >
-              Logout
-            </Button>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </Container>
   );
-}
+};
+export default Navbar;
