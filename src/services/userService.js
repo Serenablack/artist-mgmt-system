31e37c16b2c4029e8a6f9c7522abf430b263c9f8
Serena Black <sunsetsin1.sy@gmail.com>
2023-01-28 15:31:11 +0545

    feat: user create function implemented

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 export const getUser = async (token) => {
   try {
     const response = await axios.get(`api/users`, {
@@ -20,6 +21,24 @@ export const createUser = async (data) => {
       data,
     });
     return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export const createPageUser = async (data) => {
+  try {
+    if (JSON.parse(Cookies.get("userLocal"))) {
+      const token = JSON.parse(Cookies.get("userLocal")).token;
+      const response = await axios.post(`api/users`, {
+        method: "POST",
+        data,
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      });
+      return response.data;
+    }
   } catch (error) {
     return error.response.data;
   }
