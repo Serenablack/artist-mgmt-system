5fbab2ca8900b6c6cffa0122716e6627f702d06a
Serena Black <sunsetsin1.sy@gmail.com>
2023-01-28 13:00:52 +0545

    refactor: spaces removed

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -2,12 +2,10 @@ import axios from "axios";
 
 export const loginUser = async (data) => {
   try {
-    console.log(data);
     const response = await axios.post(`api/login`, {
       method: "POST",
       data,
     });
-
     return response.data;
   } catch (error) {
     return error.response.data;
