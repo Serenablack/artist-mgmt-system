f8196b9fe4916de522f94898a9475e24279aaba6
Serena Black <sunsetsin1.sy@gmail.com>
2023-01-28 13:34:46 +0545

    refactor: navbar added to index and authorized access

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,26 @@
+import Navbar from "@/components/Navbar";
 import Dashboard from "@/sections/Dashboard";
+import { Typography } from "@mui/material";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function DashboardPage() {
-  return <Dashboard />;
+  const { push } = useRouter();
+  useEffect(() => {
+    const userLocal = Cookies.get("userLocal");
+    if (!userLocal) {
+      push("/");
+    }
+  }, []);
+  const userLocal = Cookies.get("userLocal");
+
+  if (userLocal) {
+    return (
+      <>
+        <Navbar />
+        <Dashboard />
+      </>
+    );
+  }
 }
